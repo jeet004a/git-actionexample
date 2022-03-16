273ec27238e26da7d56ad9d56816b2b158c272b8
wastijeet110@gmail.com <ClinkRatCheat>
2022-03-16 23:44:04 +0530

    hello

--- src/test.py
+++ src/test.py
@@ -13,7 +13,7 @@ class FlaskAppTest(unittest.TestCase):
     
     def test_home_status_code1(self):
         """Assert that user successfully lands on homepage"""
-        tester= app.test_client(self)
+        tester= app.test_client(self) 
         response = tester.get('/jeet')
         self.assertEqual(response.status_code, 401) 
 
