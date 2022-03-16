489fc1a90612721ec909f2e727c83e9c748ae8d9
wastijeet110@gmail.com <ClinkRatCheat>
2022-03-16 22:16:55 +0530

    latest

--- src/test.py
+++ src/test.py
@@ -1,21 +1,20 @@
 from urllib import response
 from app import app
 import unittest
-import requestes
+
 
 class FlaskAppTest(unittest.TestCase):
     
 
     def test_home_status_code(self):
-        """Assert that user successfully lands on homepage"""
         tester= app.test_client(self)
-        response = tester.get('/', content_type='html/text')
+        response = tester.get('/')
         self.assertEqual(response.status_code, 200) 
     
     def test_home_status_code1(self):
         """Assert that user successfully lands on homepage"""
         tester= app.test_client(self)
-        response = tester.get('/cloudbuild', content_type='html/text')
+        response = tester.get('/jeet')
         self.assertEqual(response.status_code, 200) 
 
     
