5fb1588112ad12bbc606f7465e13475bd8995d8e
wastijeet110@gmail.com <ClinkRatCheat>
2022-03-16 21:39:10 +0530

    some changes on py.app

--- src/test.py
+++ src/test.py
@@ -1,12 +1,20 @@
-from unittest import result
-from urllib import response
 from app import app
 import unittest
-class MyTestCase(unittest.TestCase):
-    def jack(self):
-        result=self.app.get("/")
-        self.assertEqual(result.status_code,200)
 
 
-if __name__=="__main__":
+class FlaskAppTest(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def tearDown(self):
+        pass
+
+    def test_home_status_code(self):
+        """Assert that user successfully lands on homepage"""
+        result = self.app.get('/')
+        self.assertEqual(result.status_code, 200)
+
+
+if __name__ == '__main__':
     unittest.main()
