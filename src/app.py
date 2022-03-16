72d538d51fbed31042d719baed31f9c567769b6d
wastijeet110@gmail.com <ClinkRatCheat>
2022-03-16 22:06:26 +0530

    git ok

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ app=Flask(__name__)
 @app.route("/")
 def index():
     return "<h1>hello world</h1>"
-#mmfggmffe    
+#mmfggmffe     
    
 if __name__=="__main__":
     app.run()
--- src/test.py
+++ src/test.py
@@ -1,6 +1,7 @@
+from urllib import response
 from app import app
 import unittest
-
+import requestes
 
 class FlaskAppTest(unittest.TestCase):
     def setUp(self):
@@ -18,7 +19,8 @@ class FlaskAppTest(unittest.TestCase):
     def test_home_status_code1(self):
         """Assert that user successfully lands on homepage"""
         result = self.app.get('/jeet')
-        self.assertEqual(result.status_code, 200)
+        response=requests.app.get('/jeet')
+        self.assertEqual(response.status_code, 200)
 
     
 
