5c2fb013d51e10626b6221eecf7a7ac3f17ee765
wastijeet110@gmail.com <ClinkRatCheat>
2022-03-16 22:13:04 +0530

    latest

--- src/test.py
+++ src/test.py
@@ -4,17 +4,13 @@ import unittest
 import requestes
 
 class FlaskAppTest(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-        self.app.testing = True
-
-    def tearDown(self):
-        pass
+    
 
     def test_home_status_code(self):
         """Assert that user successfully lands on homepage"""
-        result = self.app.get('/')
-        self.assertEqual(result.status_code, 200)
+        tester= app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200) 
     
     def test_home_status_code1(self):
         """Assert that user successfully lands on homepage"""
