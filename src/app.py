3474541e9ee75f7e0ee3f8098824a09cdbdf2473
wastijeet110@gmail.com <ClinkRatCheat>
2022-03-25 11:18:14 +0530

    final test

--- src/app.py
+++ src/app.py
@@ -9,9 +9,9 @@ def index():
     return "<h1>this is chat boot here </h1>"
 
 
-# @app.route("/jeet")
-# def index1():
-#     return "<h1>this is nikhil boot here </h1>"
+@app.route("/jeet")
+def index1():
+    return "<h1>this is nikhil boot here </h1>"
 
 
 # mmfggmffe
