d6d7ba8734cb6c8952d46a1f88303969f580931e
wastijeet110@gmail.com <ClinkRatCheat>
2022-03-25 14:12:06 +0530

    my final commit on test.py

--- src/test.py
+++ src/test.py
@@ -16,11 +16,11 @@ class FlaskAppTest(unittest.TestCase):
         response = tester.get("/jeet")
         self.assertEqual(response.status_code, 200)
 
-    def test_home_status_code2(self):
-        """Assert that user successfully lands on homepage"""
-        tester = app.test_client(self)
-        response = tester.get("/ilp")
-        self.assertEqual(response.status_code, 200)
+    # def test_home_status_code2(self):
+    #     """Assert that user successfully lands on homepage"""
+    #     tester = app.test_client(self)
+    #     response = tester.get("/ilp")
+    #     self.assertEqual(response.status_code, 200)
 
 
 if __name__ == "__main__":
