1588f2957efe4cbe296ca71fd99d633768bee18d
wastijeet110@gmail.com <ClinkRatCheat>
2022-03-25 11:03:42 +0530

    My last commit

--- src/app.py
+++ src/app.py
@@ -1,18 +1,20 @@
 from flask import Flask
 
 
+app = Flask(__name__)
 
-app=Flask(__name__)
 
 @app.route("/")
 def index():
     return "<h1>this is chat boot here </h1>"
 
-# @app.route("/jeet")
-# def index1():
-#     return "<h1>this is nikhil boot here </h1>"
 
-#mmfggmffe     
-   
-if __name__=="__main__":
+@app.route("/jeet")
+def index1():
+    return "<h1>this is nikhil boot here </h1>"
+
+
+# mmfggmffe
+
+if __name__ == "__main__":
     app.run()
--- src/test.py
+++ src/test.py
@@ -4,19 +4,17 @@ import unittest
 
 
 class FlaskAppTest(unittest.TestCase):
-    
-
     def test_home_status_code(self):
-        tester= app.test_client(self)
-        response = tester.get('/')
-        self.assertEqual(response.status_code, 200) 
-    
+        tester = app.test_client(self)
+        response = tester.get("/")
+        self.assertEqual(response.status_code, 200)
+
     def test_home_status_code1(self):
         """Assert that user successfully lands on homepage"""
-        tester= app.test_client(self) 
-        response = tester.get('/jeet')
-        self.assertEqual(response.status_code, 200) 
-    
+        tester = app.test_client(self)
+        response = tester.get("/jeet")
+        self.assertEqual(response.status_code, 200)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
